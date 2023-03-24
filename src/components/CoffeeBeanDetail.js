9e4664b0224f6be3c062acbe02a58077d44c0fda
Grace Lee <gracelee9901@gmail.com>
2023-03-23 23:40:17 -0700

    Add functionality to sell coffee beans

--- src/components/CoffeeBeanDetail.js
+++ src/components/CoffeeBeanDetail.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function CoffeeBeanDetail(props) {
   const { bean, onClickingDelete } = props;
+  const [coffeeLeft, setCoffeeLeft] = useState(130);
+
+  function sellCoffeeBean() {
+    setCoffeeLeft(prevCoffeeLeft => prevCoffeeLeft - 1);
+  }
 
   return (
     <>
@@ -10,8 +15,10 @@ function CoffeeBeanDetail(props) {
       <h3>{bean.name} - ${bean.price}/lb</h3>
       <p>origin: {bean.origin}</p>
       <p>roast: {bean.roast}</p>
+      <p>Coffee Bean available for purchase: {coffeeLeft === 0 ? "Sold Out" : `${coffeeLeft}lbs`}</p>
+      <button disabled={coffeeLeft === 0} onClick={sellCoffeeBean}>Sell 1 Pound of Coffee Bean</button>
       <button onClick={ props.onClickingEdit }>Update Coffee Bean Detail</button>
-      <button onClick={()=> onClickingDelete(bean.id) }>Delete Coffee Bean</button>
+      <button onClick={() => onClickingDelete(bean.id) }>Delete Coffee Bean</button>
       <hr/>
     </>
   );
