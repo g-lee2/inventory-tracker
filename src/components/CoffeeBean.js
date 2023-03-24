e764990dfa47ebfc78cad3eedac7dae3453c5eed
Grace Lee <gracelee9901@gmail.com>
2023-03-23 22:46:35 -0700

    Update coffeebean component and add proptypes

--- src/components/CoffeeBean.js
+++ src/components/CoffeeBean.js
@@ -1,7 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function CoffeeBean() {
-
+function CoffeeBean(props) {
+  return (
+    <>
+      <div onClick = {() => props.whenBeanClicked(props.id)}>
+        <h3>{props.name} - ${props.price}/lb</h3>
+        <p>{props.origin}</p>
+        <p>{props.roast}</p>
+        <hr/>
+      </div>
+    </>
+  )
 }
+
+CoffeeBean.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.number,
+  roast: PropTypes.string,
+  id: PropTypes.string, 
+  whenBeanClicked: PropTypes.func 
+};
 
 export default CoffeeBean;
