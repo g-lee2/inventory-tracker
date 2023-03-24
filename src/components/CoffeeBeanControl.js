0c94533291e7e54af96f972ef9926fb443069a4b
Grace Lee <gracelee9901@gmail.com>
2023-03-23 20:45:04 -0700

    Create method to handle form in CoffeeBeanControl and pass down to form component

--- src/components/CoffeeBeanControl.js
+++ src/components/CoffeeBeanControl.js
@@ -18,12 +18,18 @@ class CoffeeBeanControl extends Component {
       formVisibleOnPage: !prevState.formVisibleOnPage
     }));
   }
+
+  handleAddingNewCoffeeBean = (newBean) => {
+    const newMainCoffeeBeanList = this.state.mainCoffeeBeanList.concat(newBean);
+    this.setState({mainCoffeeBeanList: newMainCoffeeBeanList,
+                  formVisibleOnPage: false });
+  }
   
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewCoffeeBeanForm />;
+      currentlyVisibleState = <NewCoffeeBeanForm onNewBeanCreation={this.handleAddingNewCoffeeBean}/>;
       buttonText = "Return to Coffee Bean List"; 
     } else {
       currentlyVisibleState = <CoffeeBeanList coffeeBeanList={this.state.mainCoffeeBeanList} />;;
--- src/components/NewCoffeeBeanForm.js
+++ src/components/NewCoffeeBeanForm.js
@@ -1,9 +1,51 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
 
-function NewCoffeeBeanForm() {
+function NewCoffeeBeanForm(props) {
+
+  function handleNewCoffeeBeanFormSubmission(event) {
+    event.preventDefault();
+    props.onNewBeanCreation({
+      name: event.target.name.value, 
+      origin: event.target.origin.value, 
+      price: parseInt(event.target.price.value), 
+      roast: event.target.roast.value,
+      id: v4()
+    });
+  }
+
   return (
-    <h1>THIS IS A FORM</h1>
-  )
+    <React.Fragment>
+      <form onSubmit={handleNewCoffeeBeanFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name of Coffee' 
+        />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' 
+        />
+        <input
+          type='number'
+          name='price'
+          placeholder='Price/lb' 
+        />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast' 
+        />
+        <button type='submit'>Add Coffee Bean!</button>
+      </form>
+    </React.Fragment>
+  );
 }
+
+NewCoffeeBeanForm.propTypes = {
+  onNewBeanCreation: PropTypes.func
+};
 
 export default NewCoffeeBeanForm;
